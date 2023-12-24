55bc2b504be58b7f3f5f46217287379455782dd6
MilfordIndBaptistChurch <jamess.developer+mibc@gmail.com>
2023-12-24 15:26:00 +1300

    Proposed side nav

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,40 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div>
-    <Header title="Milford Bible Companion" />
-  </div>
-
-  <RouterView />
+	<div class="grid">
+		<div>&nbsp;</div>
+		<div class="middle">
+	    <Header title="Milford Bible Companion" />
+			<RouterView />
+		</div>
+		<div class="side-nav">
+			<aside class="menu">
+			  <p class="menu-label">
+			    General
+			  </p>
+			  <ul class="menu-list">
+			    <li><a>Books (5)</a></li>
+			    <li><a>Cross ref (10)</a></li>
+			    <li><a>Accordance (0)</a></li>
+			  </ul>
+			  <p class="menu-label">
+			    Application
+			  </p>
+			  <ul class="menu-list">
+			    <li><a>Sermons (0)</a></li>
+			    <li><a>Illustrations (0)</a></li>
+			    <li><a>Characters (4)</a></li>
+			    <li><a>History (0)</a></li>
+			  </ul>
+			  <p class="menu-label">
+			    Extra
+			  </p>
+			  <ul class="menu-list">
+			    <li><a>Bible Studies (0)</a></li>
+			    <li><a>Discussions (0)</a></li>
+			    <li><a>Notes (0)</a></li>
+			  </ul>
+			</aside>
+		</div>
+	</div>
 </template>
