ae038d028f8d785740f6143d6ccb7539607bf2a2
MilfordIndBaptistChurch <jamess.developer+mibc@gmail.com>
2023-12-25 17:46:54 +1300

    Add counts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 	import { RouterLink, RouterView } from 'vue-router'
 	import Header from './components/Header.vue'
+  import dataSource from './common/data.json';
 </script>
 
 <template>
@@ -16,8 +17,8 @@
 			    General
 			  </p>
 			  <ul class="menu-list">
-			    <li><a>Books (5)</a></li>
-			    <li><a>Cross ref (10)</a></li>
+			    <li><a>Books ({{bookCount}})</a></li>
+			    <li><a>Cross ref ({{crossRefCount}})</a></li>
 			    <li><a>Accordance (0)</a></li>
 			  </ul>
 			  <p class="menu-label">
@@ -26,7 +27,7 @@
 			  <ul class="menu-list">
 			    <li><a>Sermons (0)</a></li>
 			    <li><a>Illustrations (0)</a></li>
-			    <li><a>Characters (4)</a></li>
+			    <li><a>Characters ({{charactersCount}})</a></li>
 			    <li><a>History (0)</a></li>
 			  </ul>
 			  <p class="menu-label">
@@ -41,3 +42,61 @@
 		</div>
 	</div>
 </template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import { getRef } from './common/utils';
+
+  const bookCount = ref(0);
+  const crossRefCount = ref(0);
+  const charactersCount = ref(0)
+
+	const getUniqueValues = (array: []) => (
+	  array.filter((currentValue, index, arr) => (
+			arr.indexOf(currentValue) === index
+		))
+	)
+
+	export default defineComponent({
+    methods: {
+	    // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type ** resolves this for now **
+	    getIndex(obj: any, string: any) {
+	    	return obj[string]!; // will never be undefined!
+	    },
+      setCounts () {
+        let bookArray = [] as Array<String>;
+        let crossRefArray = [] as Array<String>;
+        let charactersArray = [] as Array<String>;
+        for (const i in dataSource) {
+        	const book = getRef(dataSource[Number(i)]).book;
+        	bookArray.push(book);
+        	bookArray = getUniqueValues(bookArray as []);
+
+        	// cross ref count
+        	const stringRef = Object.keys(dataSource[Number(i)])[0];
+        	const index = this.getIndex(dataSource[Number(i)], stringRef);
+        	const study = index.study;
+
+        	for (const j in study) {
+        		if (study[j].methods.hasOwnProperty('cross references')) {
+        			const makeArray = study[j].methods['cross references'].split(',');
+        			const mergeArrays = crossRefArray.concat(makeArray);
+        			crossRefArray = getUniqueValues(mergeArrays as []);
+        		} else if (study[j].methods.hasOwnProperty('characters')) {
+        			const makeArray = study[j].methods['characters'].split(',');
+        			const mergeArrays = charactersArray.concat(makeArray);
+        			charactersArray = getUniqueValues(mergeArrays as []);
+        		}
+        	}
+        }
+
+       	bookCount.value = bookArray.length;
+        crossRefCount.value = crossRefArray.length;
+        charactersCount.value = charactersArray.length;
+      },
+    },
+    beforeMount() {
+      this.setCounts()
+    }
+	});
+</script>
