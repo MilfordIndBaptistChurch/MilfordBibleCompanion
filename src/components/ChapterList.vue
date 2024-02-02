d29fcc8105fd5d2beaf67d4301166e92b3a9e3dd
MilfordIndBaptistChurch <jamess.developer+mibc@gmail.com>
2024-02-02 18:47:30 +1300

    Add highlight

--- src/components/ChapterList.vue
+++ src/components/ChapterList.vue
@@ -13,25 +13,40 @@
 	:data-source="bookRef.chapter"
 	size="small"
 	v-if="showChapterState.checked"
+	style="margin: 25px 0 0 0"
 	>
 	<template #renderItem="{ item }">
-	  <a-list-item class="list">
-	  <a-list-item-meta
-	    description=""
-	  >
-	    <template #title>
-	    	<h4 class="ant-list-item-meta-title">
-	    		<a href="">#{{ item.verse }}</a>
-			  	<a-tooltip :open="isOpen(item)">
-			  		<template #title>Copied</template>
-			  		<div class="tooltip-2">
-			  			<font-awesome-icon icon="fa-far fa-copy" @click="handleCopy(item)" />
-			  		</div>
-			  	</a-tooltip>
-	    	</h4>
-	    </template>
-	  </a-list-item-meta>
-	      {{ item.text }}
+	  <a-list-item v-if="Number(bookRef.selected.verse) === Number(item.verse)" class="list" style="background: #eff5fb">
+		  <a-list-item-meta>
+		    <template #title>
+		    	<h4 class="ant-list-item-meta-title">
+		    		<a href="">#{{ item.verse }}</a>
+				  	<a-tooltip :open="isOpen(item)">
+				  		<template #title>Copied</template>
+				  		<div class="tooltip-2">
+				  			<font-awesome-icon icon="fa-far fa-copy" @click="handleCopy(item)" />
+				  		</div>
+				  	</a-tooltip>
+		    	</h4>
+		    </template>
+		  </a-list-item-meta>
+    	{{ item.text }}{{ bookRef.selected.verse === Number(item.verse) }}
+	  </a-list-item>
+		<a-list-item v-if="Number(bookRef.selected.verse) !== Number(item.verse)" class="list">
+		  <a-list-item-meta>
+		    <template #title>
+		    	<h4 class="ant-list-item-meta-title">
+		    		<a href="">#{{ item.verse }}</a>
+				  	<a-tooltip :open="isOpen(item)">
+				  		<template #title>Copied</template>
+				  		<div class="tooltip-2">
+				  			<font-awesome-icon icon="fa-far fa-copy" @click="handleCopy(item)" />
+				  		</div>
+				  	</a-tooltip>
+		    	</h4>
+		    </template>
+		  </a-list-item-meta>
+    	{{ item.text }}{{ bookRef.selected.verse }} {{ Number(item.verse) }}
 	  </a-list-item>
 	</template>
 	</a-list>
